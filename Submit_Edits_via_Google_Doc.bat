f36086eb626fda832c6907330b19933afc5dc6bd
BenevolenceMessiah <120072200+BenevolenceMessiah@users.noreply.github.com>
2023-08-06 22:54:58 -0400

    Add files via upload

---
+++ Submit_Edits_via_Google_Doc.bat
@@ -0,0 +1,3 @@
+# Use this to open the Google Doc for collaborative editing. Credit yourself accordingly. Commits/Edits in the Google Doc that are chosen as contributions to the theory will be added to the Main Branch Repo (https://github.com/BenevolenceMessiah/Peoples_War_Adapted_for_Modern_Western_Conditions_Artificial_Intelligence_AI).
+
+start https://docs.google.com/document/d/1avOPflrowbKm3s7IJzJlwo00SpdF9wtgKU7UgdY6hSA/edit?usp=sharing
